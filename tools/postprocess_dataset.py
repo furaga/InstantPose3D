c56358e246fa284721352a7850fa965637c2b33c
furaga <furaga.fukahori@gmail.com>
2022-08-17 18:03:48 +0900

    fix projection

--- tools/postprocess_dataset.py
+++ tools/postprocess_dataset.py
@@ -43,6 +43,47 @@ def parse_args():
     parser.add_argument("--root_dir", type=Path, required=True)
     return parser.parse_args()
 
+def create_translation_mat(x: float, y: float, z: float):
+    trans_mat = np.eye(4)
+    trans_mat[:, 3] = [x, y, z, 1]
+
+    return trans_mat
+
+
+def create_scale_mat(x: float, y: float, z: float):
+    scale_mat = np.eye(4)
+
+    scale_mat[0, 0] = x
+    scale_mat[1, 1] = y
+    scale_mat[2, 2] = z
+
+    return scale_mat
+
+def create_look_at_mat(eye, target, up=[0.0, 0.0, 1.0]):
+    eye = np.array(eye, dtype=np.float32, copy=True)
+    target = np.array(target, dtype=np.float32, copy=True)
+    up = np.array(up, dtype=np.float32, copy=True)
+
+    z = eye - target
+    z = z / np.linalg.norm(z)
+
+    x = np.cross(up, z)
+    x = x / np.linalg.norm(x)
+
+    y = np.cross(z, x)
+    y = y / np.linalg.norm(y)
+
+    rotate_mat = np.array([
+        [x[0], x[1], x[2], 0],
+        [y[0], y[1], y[2], 0],
+        [z[0], z[1], z[2], 0],
+        [0, 0, 0, 1]
+    ])
+    
+    scale_mat = create_scale_mat(1, -1, -1)
+    tmp = np.dot(scale_mat, rotate_mat)
+
+    return tmp[:3, :3]
 
 def load_param(param_path):
     mtx = np.eye(3)
@@ -61,10 +102,9 @@ def load_param(param_path):
                 extrinsic_euler = np.array([float(t) for t in tokens[3 : 3 + 3]])
             elif tokens[0] == "intrinsic":
                 mtx = np.array([float(t) for t in tokens[3 : 3 + 9]]).reshape(3, 3)
+                mtx[1, 1] = mtx[0, 0]
             else:
-                R = scipy.spatial.transform.Rotation.from_euler(
-                    "xzy", extrinsic_euler
-                ).as_matrix()
+                R = create_look_at_mat(extrinsic[:3, 3], (0, 0, 1))
                 p = extrinsic[:3, 3]
                 p = -np.matmul(R, p.T).T
                 pose = np.zeros((3, 4), float)
@@ -93,7 +133,6 @@ def convert_param(params):
         kp = cv2.projectPoints(np.array([pt]), pose[:3, :3], pose[:3, 3], mtx, None)[0][
             0
         ].ravel().astype(int)
-        # transform 3d point
         new_pt = pose @ np.append(pt, 1).T
         new_params[target] = *new_pt, *kp
 
@@ -117,22 +156,41 @@ def main(args):
     
     bbox = np.array([np.inf, np.inf, np.inf]), np.array([-np.inf, -np.inf, -np.inf])
 
+    latest_ng = None
     for i, img_path in enumerate(all_img_paths):
+        if i % 1000 == 0:
+            print("{}/{}".format(i, len(all_img_paths)))
+        if latest_ng == img_path.parent:
+            continue
+        
         sub_name = img_path.parent.name
         param_path = args.root_dir / "PARAMS_RAW" / sub_name / (img_path.stem + ".txt")
         params = load_param(param_path)
         new_params = convert_param(params)
         
-        #render = visualize_keypoint(cv2.imread(str(img_path)), new_params)
-        #cv2.imshow("render", render)
-        #cv2.waitKey(0)
-        
-        out_param_path = args.root_dir / "PARAMS" / sub_name / (img_path.stem + ".txt")
-        out_param_path.parent.mkdir(parents=True, exist_ok=True)
-        save_param(out_param_path, new_params)
-        
+        n_ok = True
         for _, kp in new_params.items():
-            bbox = np.minimum(bbox[0], kp[:3]), np.maximum(bbox[1], kp[:3])
+            if kp[3] < 0 or 448 <= kp[3] or kp[4] < 0 or 448 <= kp[4]:
+                pass
+            else:
+                n_ok += 1
+                
+        # 二個ミスなら許容
+        if n_ok >= len(target_bone_names) - 2:
+            # out_param_path = args.root_dir / "PARAMS" / sub_name / (img_path.stem + ".txt")
+            # out_param_path.parent.mkdir(parents=True, exist_ok=True)
+            # save_param(out_param_path, new_params)
+            
+            for _, kp in new_params.items():
+                bbox = np.minimum(bbox[0], kp[:3]), np.maximum(bbox[1], kp[:3])
+        else:
+            latest_ng = img_path.parent
+            print(f"{str(img_path)} has out-of-range keypoints. Skip the remaining images.")
+            
+            render = visualize_keypoint(cv2.imread(str(img_path)), new_params)
+            cv2.imshow("render", render)
+            cv2.waitKey(0)
+        
 
     print(bbox)
 
